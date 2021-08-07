944505508ac39bc98875d27600c586b9cb07816a
Glauber Matteis Gadelha <glaubergad@gmail.com>
2021-08-07 14:42:29 -0300

    first commit

---
+++ src/br/net/glaubergad/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.net.glaubergad;
+
+import br.net.glaubergad.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato("chaninho","marrom","2");
+		
+		int a= 2;
+		int b= 3;
+		System.out.println("Hello World! " + (a+b));
+		System.out.println(gato);
+	}
+
+}
